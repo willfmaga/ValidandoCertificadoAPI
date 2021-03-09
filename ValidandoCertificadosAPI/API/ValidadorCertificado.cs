0f727bfe3cdc325323addd86065c91a048991e7c
William F Magalhães <william.magalhaes@edenred.com>
2021-03-09 17:28:05 -0300

    pesquisando os certificados instalados na máquina

--- ValidandoCertificadosAPI/API/ValidadorCertificado.cs
+++ ValidandoCertificadosAPI/API/ValidadorCertificado.cs
@@ -11,7 +11,8 @@ namespace API
         internal bool Validar(X509Certificate2 clientCertificate)
         {
             File.WriteAllText(@"c:\temp\certificado\teste.txt", clientCertificate.Thumbprint);
-            var certificadoParaComparar = new X509Certificate2(@"C:\temp\certificado\client_test.pfx", "19372846");
+            //var certificadoParaComparar = new X509Certificate2(@"C:\temp\certificado\client_test.pfx", "19372846");
+           var certificadoParaComparar = CarregaCertificados(clientCertificate);
 
             if (certificadoParaComparar.Thumbprint == clientCertificate.Thumbprint)
             {
@@ -25,6 +26,16 @@ namespace API
         {
             File.WriteAllText(@"c:\temp\certificado\teste_erro.txt", JsonConvert.SerializeObject(context));
             Console.WriteLine(context);
+        }
+
+        internal X509Certificate2 CarregaCertificados(X509Certificate2 clientCertificate)
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadWrite);
+
+            var certificates = new X509Certificate2Collection(store.Certificates);
+
+           return  certificates.Find(X509FindType.FindByIssuerName, clientCertificate.IssuerName, true)[0];
         }
     }
 }
